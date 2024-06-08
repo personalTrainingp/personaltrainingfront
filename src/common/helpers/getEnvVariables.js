e417fc8eee1aa11db7d381d6532af381a4865de1
personalTrainingp <crosales@personaltraining.com.pe>
2024-06-08 09:38:22 -0500

    editando getEnvVariable para test

--- src/common/helpers/getEnvVariables.js
+++ src/common/helpers/getEnvVariables.js
@@ -1,8 +1,15 @@
 const getEnvVariables = () => {
-	import.meta.env;
+	// import.meta.env;
 
-	return {
-		...import.meta.env,
-	};
+	// return {
+	// 	...import.meta.env,
+	// };
+	const API_URL = import.meta.env.VITE_API_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  return {
+    API_URL,
+    API_KEY,
+  };
 };
 export default getEnvVariables;
