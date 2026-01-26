9ecb776456f53883952a3bc1a10beb8897c022ce
personalTrainingp <crosales@personaltraining.com.pe>
2026-01-26 10:57:26 -0500

    2024 generarmeses

--- src/pages/pagePT/reportes/resumenEjecutivo/Pages/ComparativoConMes/AppComparativoConMes.jsx
+++ src/pages/pagePT/reportes/resumenEjecutivo/Pages/ComparativoConMes/AppComparativoConMes.jsx
@@ -16,11 +16,11 @@ export const AppComparativoConMes = () => {
   return (
     <div>
         <FechaCorte corte={corte.corte} inicio={corte.inicio}/>
-        <DataTable1 nombreCategoriaVenta={'MEMBRESIAS'} data={dataVentas.dataMembresias?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2025-09-01 15:45:47.6640000 +00:00')}/>
-        <DataTable1 nombreCategoriaVenta={'SUPLEMENTOS'} data={dataVentas.dataProductos17?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2025-09-01 15:45:47.6640000 +00:00')}/>
-        <DataTable1 nombreCategoriaVenta={'ACCESORIOS'} data={dataVentas.dataProductos18?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2025-09-01 15:45:47.6640000 +00:00')}/>
-        <DataTable1 nombreCategoriaVenta={'MONKEY FIT'} data={dataVentas.dataMFMap?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2025-09-01 15:45:47.6640000 +00:00')}/>
-        <DataTable1 nombreCategoriaVenta={'TOTAL'} data={totalMap?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2025-09-01 15:45:47.6640000 +00:00')}/>
+        <DataTable1 nombreCategoriaVenta={'MEMBRESIAS'} data={dataVentas.dataMembresias?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2024-09-01 15:45:47.6640000 +00:00')}/>
+        <DataTable1 nombreCategoriaVenta={'SUPLEMENTOS'} data={dataVentas.dataProductos17?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2024-09-01 15:45:47.6640000 +00:00')}/>
+        <DataTable1 nombreCategoriaVenta={'ACCESORIOS'} data={dataVentas.dataProductos18?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2024-09-01 15:45:47.6640000 +00:00')}/>
+        <DataTable1 nombreCategoriaVenta={'MONKEY FIT'} data={dataVentas.dataMFMap?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2024-09-01 15:45:47.6640000 +00:00')}/>
+        <DataTable1 nombreCategoriaVenta={'TOTAL'} data={totalMap?.filter((el) => corte.dia.includes(el.dia))} arrayFechas={generarMesYanio('2024-09-01 15:45:47.6640000 +00:00')}/>
     </div>
   )
 }
