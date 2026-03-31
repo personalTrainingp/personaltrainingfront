af0aacd139f3bab201357a1f1fcafd6b03ff0dbe
Carlos Rosales <carlosrosales21092002@hotmail.com>
2026-03-31 16:25:42 -0500

    proy orden de compra

--- src/pages/pagePT/ordenCompra/AppOrdenCompra.jsx
+++ src/pages/pagePT/ordenCompra/AppOrdenCompra.jsx
@@ -7,7 +7,7 @@ export const AppOrdenCompra = ({id_empresa}) => {
       const { dataView } = useSelector(e=>e.EGRESOS)
   return (
     <div>
-        <div className='fs-2 text-change'>IGV ACUMULADO EN TOTAL: <NumberFormatMoney amount={dataView.reduce((total, item)=>item.monto+total, 0)}/></div>
+        <div className='fs-2 text-change'>IGV ACUMULADO EN TOTAL: <NumberFormatMoney amount={dataView.reduce((total, item)=>item.monto+total, 0)-(dataView.reduce((total, item)=>item.monto+total, 0)/1.18)}/></div>
         <DataTableGastos sonCompras={true} id_empresa={id_empresa} />
     </div>
   )
