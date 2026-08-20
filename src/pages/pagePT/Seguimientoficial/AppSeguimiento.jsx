d008f8ca445b622dfc4e179770dba48d0cd534a9
carlosrosales101001011 <carlosrosales21092002@hotmail.com>
2026-08-20 08:28:16 -0500

    seguimiento, nuevo real

--- src/pages/pagePT/Seguimientoficial/AppSeguimiento.jsx
+++ src/pages/pagePT/Seguimientoficial/AppSeguimiento.jsx
@@ -2,21 +2,98 @@ import React, { useEffect } from 'react'
 import { useSeguimientoStore } from './useSeguimientoStore'
 import { Col, Row } from 'react-bootstrap'
 import { TableSeguimientos } from './TableSeguimientos'
-
 export const AppSeguimiento = () => {
-    const { obtenerSeguimientoxFecha, dataSeguimientoxFecha } = useSeguimientoStore()
-    useEffect(() => {
-        obtenerSeguimientoxFecha()
-    }, [])
-    const fecha = new Date()
+    const { obtenerSeguimientoxFecha, dataSeguimientoxFecha } = useSeguimientoStore();
 
-  return (
-    <div className='tab-scroll-container'>
-      <div className='fs-1 fw-bold text-change d-flex flex-row'>
-        <TableSeguimientos bodyHeadcontadorDia={<>SESIONES <br/> PENDIENTES</>} dataSeguimientoxFecha={dataSeguimientoxFecha} title={<><span className='text-change'>SOCIOS ACTIVOS </span></>} rangeDate={["2026-08-04T12:00:00.000Z", "2040-03-16T12:00:00.000Z"]}/>
-        <TableSeguimientos bodyHeadcontadorDia={<>DIAS <br/> VENCIDOS</>} dataSeguimientoxFecha={dataSeguimientoxFecha} title={<><span className='text-change'>RENOVACIONES VENCIDAS</span></>} rangeDate={[ "2026-05-04T12:00:00.000Z","2026-08-04T12:00:00.000Z"]}/>
-        <TableSeguimientos bodyHeadcontadorDia={<>DIAS <br/> VENCIDOS</>} dataSeguimientoxFecha={dataSeguimientoxFecha} title={<><span className='text-change'>REINSCRIPCIONES VENCIDAS</span></>} rangeDate={["2024-09-16T12:00:00.000Z", "2026-05-04T12:00:00.000Z"]}/>
-      </div>
-    </div>
-  )
-}
+    useEffect(() => {
+        obtenerSeguimientoxFecha();
+    }, []);
+
+    const fechaActual = new Date();
+
+    // Fecha actual
+    const fechaActualStr = fechaActual.toISOString();
+
+    // Fecha de hace 3 meses
+    const fechaInicioRenovaciones = new Date(fechaActual);
+    fechaInicioRenovaciones.setMonth(fechaInicioRenovaciones.getMonth() - 3);
+
+    // Formatear fechas
+    const fechaInicioRenovacionesStr = fechaInicioRenovaciones.toISOString();
+
+    // Reinscripciones: desde 2024 hasta el día anterior al inicio de renovaciones
+    const fechaInicioReinscripciones = "2024-01-01T12:00:00.000Z";
+
+    const fechaFinReinscripciones = fechaInicioRenovaciones.toISOString();
+
+    return (
+        <div className="tab-scroll-container">
+            <div className="fs-1 fw-bold text-change d-flex flex-row">
+
+                {/* SOCIOS ACTIVOS */}
+                <TableSeguimientos
+                    bodyHeadcontadorDia={
+                        <>
+                            SESIONES <br /> PENDIENTES
+                        </>
+                    }
+                    dataSeguimientoxFecha={dataSeguimientoxFecha}
+                    title={
+                        <>
+                            <span className="text-change">
+                                SOCIOS ACTIVOS
+                            </span>
+                        </>
+                    }
+                    rangeDate={[
+                        fechaActualStr,
+                        "2040-03-16T12:00:00.000Z"
+                    ]}
+                />
+
+                {/* RENOVACIONES */}
+                <TableSeguimientos
+                    bodyHeadcontadorDia={
+                        <>
+                            DIAS <br /> VENCIDOS
+                        </>
+                    }
+                    dataSeguimientoxFecha={dataSeguimientoxFecha}
+                    title={
+                        <>
+                            <span className="text-change">
+                                RENOVACIONES VENCIDAS
+                            </span>
+                        </>
+                    }
+                    rangeDate={[
+                        fechaInicioRenovacionesStr,
+                        fechaActualStr
+                    ]}
+                />
+
+                {/* REINSCRIPCIONES */}
+                <TableSeguimientos
+                    bodyHeadcontadorDia={
+                        <>
+                            DIAS <br /> VENCIDOS
+                        </>
+                    }
+                    dataSeguimientoxFecha={dataSeguimientoxFecha}
+                    title={
+                        <>
+                            <span className="text-change">
+                                REINSCRIPCIONES VENCIDAS
+                            </span>
+                        </>
+                    }
+                    rangeDate={[
+                        fechaInicioReinscripciones,
+                        fechaFinReinscripciones
+                    ]}
+                />
+
+            </div>
+        </div>
+    );
+};
