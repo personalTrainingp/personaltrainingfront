b41b802e1595d72d2752780ddfe5ab454167cd13
carlosrosales101001011 <carlosrosales21092002@hotmail.com>
2026-09-21 10:39:25 -0500

    proyecto ral en carlos ch

--- src/pages/pagePT/GestInventario/GestionInventario.jsx
+++ src/pages/pagePT/GestInventario/GestionInventario.jsx
@@ -73,6 +73,9 @@ export const GestionInventario = () => {
             </TabPanel>
             <TabPanel header='CARLOS CH.'>
                   <TabView>
+                        <TabPanel header={<>PROYECTO RAL</>}>
+                              <DataInventario id_enterprice={2109599} id_zona={599}/>
+                        </TabPanel>
                         <TabPanel header={<>REDUCTO<br/>INVENTARIO TOTAL</>}>
                               <DataInventario id_enterprice={1599} id_zona={599}/>
                         </TabPanel>
